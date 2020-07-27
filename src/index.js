5f44102ad2ec346cce2266a2cd9c49cee002fbd2
amir-shariati <seyed.amir.shariati@gmail.com>
2020-07-27 15:01:40 +0430

    component with props is added to render

--- src/index.js
+++ src/index.js
@@ -2,14 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function hello(props) {
+function Hello(props) {
     return(
         <h1> hello, {props.name}</h1>
     )
 }
 
 ReactDOM.render(
-    hello({name: "amir"}),
+    <Hello name="Ali"></Hello>,
     document.getElementById('root')
 )
+
 
