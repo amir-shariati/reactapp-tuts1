1b64766f2c3130b78f35094dee6bfa71e4b2ffae
amir-shariati <seyed.amir.shariati@gmail.com>
2020-07-28 11:29:43 +0430

    event click test for function

--- src/index.js
+++ src/index.js
@@ -2,50 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Clock extends React.Component{
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            date: new Date().toLocaleTimeString()
-        }
-    }
-
-    tick(){
-        this.setState({date: new Date().toLocaleTimeString()});
-    }
-
-    componentDidMount() {
-        this.tickId = setInterval( ()=>this.tick(), 1000);
-    }
-
-    componentWillUnmount() {
-        clearInterval(this.tickId)
-    }
-
-    render() {
-        return(
-            <div>
-                <h1>Hello, Amir</h1>
-                <h2> It is {this.state.date}</h2>
-            </div>
-        )
-    }
-}
-
-function App(props){
-    return(
-        <div>
-            <Clock/>
-            <Clock/>
-            <Clock/>
-        </div>
+function App(props) {
+    return (
+        <button onClick={()=> alert('Clicked!')}>
+            Click Me.
+        </button>
     )
 }
 
 ReactDOM.render(
-    <App />,
+    <App/>,
     document.getElementById('root')
 )
-
-
