ad9b1a10d71cf7c22dc3b9d1d06576cf1cc4cfa2
amir-shariati <seyed.amir.shariati@gmail.com>
2020-07-28 12:12:10 +0430

    conditional rendering

--- src/index.js
+++ src/index.js
@@ -2,31 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+function User(props) {
+    return(
+        <div>
+            Hello User
+        </div>
+    )
+}
+
+function Guest(props) {
+    return(
+        <div>
+            Welcome Guest
+        </div>
+    )
+}
+
 class App extends React.Component{
     constructor(props) {
         super(props);
-
-        this.state = {
-            value: "OFF"
-        }
-
-        // this.toggleValue = this.toggleValue.bind(this);
     }
-    toggleValue() {
-       this.setState( (prevState) => ({
-           value: prevState.value == "OFF" ? "ON" : "OFF"
-       }))
-    }
+
     render() {
-        return (
-            <button onClick={(event) => this.toggleValue(this, event) }>
-                {this.state.value}
-            </button>
-        )
+        return this.props.isLoggedIn ? <User/> : <Guest/>
     }
 }
 
+
 ReactDOM.render(
-    <App/>,
+    <App isLoggedIn={true} />,
     document.getElementById('root')
 )
