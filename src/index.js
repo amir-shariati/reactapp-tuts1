cd11f305827319bde56408f2fe35ec939987bc19
amir-shariati <seyed.amir.shariati@gmail.com>
2020-07-29 14:43:12 +0430

    temp test

--- src/index.js
+++ src/index.js
@@ -2,44 +2,85 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+class BoilingVerdict extends React.Component{
+    render() {
+        return(
+            this.props.celsius>=100?
+                <p>the water would boil</p>
+                :
+                <p>the water would not boil</p>
+        )
+    }
+}
 
-class App extends React.Component{
+class TempInput extends React.Component{
     constructor(props) {
         super(props);
+    }
+
+    render() {
+        return(
+            <fieldset>
+                <legend>temperature in {this.props.scale==="f"? "Fahrenheit" : "Celsius"}</legend>
+                <input value={this.props.temperature} onChange={this.props.handleChange} />
+
+            </fieldset>
+        )
+    }
+}
+
+class Calculator extends React.Component{
+    constructor(props) {
+        super(props);
+
         this.state = {
-            name: '',
-            address: ''
+            temperatureInF: 0*9/5 + 32,
+            temperatureInC: 0
         }
 
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit=this.handleSubmit.bind(this);
+        this.handleChangeInF = this.handleChangeInF.bind(this);
+        this.handleChangeInC = this.handleChangeInC.bind(this);
     }
 
-    handleChange(event){
-        this.setState( {[event.target.name]: event.target.value.toUpperCase()});
-        console.log(this.state);
+    handleChangeInF(event){
+        this.setState({
+            temperatureInF : event.target.value,
+            temperatureInC : (event.target.value -32)*5/9
+        })
     }
 
-    handleSubmit(event){
-        event.preventDefault();
-        alert(this.state.name + ' ' + this.state.address);
+    handleChangeInC(event){
+        this.setState({
+            temperatureInF : (event.target.value)*9/5 + 32,
+            temperatureInC : event.target.value
+        })
+    }
+
+    convertToCelsius(t){
+        return (t-32)*5/9 ;
+    }
+
+    convertToFahrenheit(t){
+        return t*9/5+32 ;
     }
 
     render() {
         return(
             <div>
-                <form onSubmit={this.handleSubmit} >
-                    <label>
-                        Name:
-                        <input name={"name"} value={this.state.name} type={"text"} onChange={this.handleChange} />
-                    </label>
-                    <label>
-                        Address:
-                        <textarea name={"address"} value={this.state.address} type={"text"} onChange={this.handleChange} />
-                    </label>
+                <TempInput scale={"f"} temperature={this.state.temperatureInF}  handleChange={this.handleChangeInF} />
+                <TempInput scale={"c"} temperature={this.state.temperatureInC}  handleChange={this.handleChangeInC} />
+                <BoilingVerdict celsius={this.state.temperatureInC} />
+            </div>
+        )
+    }
+}
 
-                    <input type={"submit"} value={"Ok"} />
-                </form>
+class App extends React.Component{
+
+    render() {
+        return(
+            <div>
+                <Calculator />
             </div>
         )
     }
